a5da1f8b28f083b57ed121e6d532c46cb832e8e0
Hoang Phuc Nguyen <mnguyen@videobankdigital.com>
2025-05-02 15:04:01 -0400

    test

---
+++ Project1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESRI.ArcGIS.Client.Symbols;
+using System.Windows.Media;
+namespace Project1
+{
+    public class Project1Class1
+    {
+        public SimpleMarkerSymbol TargetSymbol =
+         new SimpleMarkerSymbol()
+         {
+             Color = new SolidColorBrush(Color.FromRgb(3, 164, 241)),
+             Size = 10,
+             Style = SimpleMarkerSymbol.SimpleMarkerStyle.Circle,
+         };
+
+    }
+}
